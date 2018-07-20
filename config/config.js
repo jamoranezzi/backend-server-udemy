a5d9196bbcd433e3ac1c7c6e75ff296463a06e51
andresmorale <andres.morales@emitekllc.com>
2018-07-19 21:47:47 -0400

    Ultimos ajustes Google login - fallo por actualización de version

--- config/config.js
+++ config/config.js
@@ -2,3 +2,6 @@ module.exports.SEED = 'este@es@un@seed@token';
 
 // Google
 module.exports.CLIENT_ID = '1041195098351-tta4ktdk881mb6ofuat0jse4olt9ai86.apps.googleusercontent.com';
+// Google
+module.exports.GOOGLE_CLIENT_ID = '1041195098351-tta4ktdk881mb6ofuat0jse4olt9ai86.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = 'xCuW0gZauCXFMTMrvstlPgaw';
